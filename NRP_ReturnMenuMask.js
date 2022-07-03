c3f503e8b4f5f082f15fe791c22f788a39ee0f89
Takeshi Sunagawa <74476616+NewRPGProject@users.noreply.github.com>
2022-07-03 16:25:45 +0900

    シーン名の指定を有効に

--- NRP_ReturnMenuMask.js
+++ NRP_ReturnMenuMask.js
@@ -3,7 +3,7 @@
 //=============================================================================
 /*:
  * @target MV MZ
- * @plugindesc v1.00 Eliminate the seam when closing the menu.
+ * @plugindesc v1.01 Eliminate the seam when closing the menu.
  * @author Takeshi Sunagawa (http://newrpg.seesaa.net/)
  * @url http://newrpg.seesaa.net/article/484101737.html
  *
@@ -20,16 +20,17 @@
  * with a "captured image of the screen just before opening the menu"
  * when the menu is closed.
  * 
- * ------------------------------------------
- * [Usage]
- * ------------------------------------------
- * You can simply apply it.
- * The plugin parameters are also basically fine
- * with the default settings.
+ * In addition, if you specify a scene name as a plugin parameter,
+ * it can be applied when returning from a scene other than the menu.
  * 
- * ------------------------------------------
+ * ※For example, you can support external message logging plugin.
+ * ※This is for somewhat advanced users,
+ *   but all you need to do is open the js of the relevant plugin
+ *   and find the name of the scene (usually named Scene_XXXX).
+ * 
+ * -------------------------------------------------------------------
  * [Terms]
- * ------------------------------------------
+ * -------------------------------------------------------------------
  * There are no restrictions.
  * Modification, redistribution freedom, commercial availability,
  * and rights indication are also optional.
@@ -40,11 +41,16 @@
  * @type number
  * @default 1
  * @desc The number of frames to hide the seam when the menu is closed.
+ * 
+ * @param SceneList
+ * @type string[]
+ * @default ["Scene_Menu"]
+ * @desc List of scenes for mask processing.
  */
 
 /*:ja
  * @target MV MZ
- * @plugindesc v1.00 メニューを閉じた際の継ぎ目をなくす。
+ * @plugindesc v1.01 メニューを閉じた際の継ぎ目をなくす。
  * @author 砂川赳（http://newrpg.seesaa.net/）
  * @url http://newrpg.seesaa.net/article/484101737.html
  *
@@ -62,16 +68,16 @@
  * 「メニューを開く直前の画面のキャプチャ画像」をかぶせることで、
  * 一瞬の切れ目をなくすのがこのプラグインです。
  * 
- * ------------------------------------------
- * ■使用方法
- * ------------------------------------------
- * 適用するだけでＯＫです。
- * プラグインパラメータも基本的には、
- * 初期設定のままで問題ないと思われます。
+ * さらにプラグインパラメータにシーン名を指定すれば、
+ * メニュー以外から戻った際にも適応できます。
  * 
- * ------------------------------------------
+ * ※例えば、外部のメッセージログ系プラグインなどに対応できます。
+ * ※やや上級者向けですが、該当のプラグインのjsを開いて
+ * 　シーン名（普通はScene_XXXXという名称）を見つければＯＫです。
+ * 
+ * -------------------------------------------------------------------
  * ■利用規約
- * ------------------------------------------
+ * -------------------------------------------------------------------
  * 特に制約はありません。
  * 改変、再配布自由、商用可、権利表示も任意です。
  * 作者は責任を負いませんが、不具合については可能な範囲で対応します。
@@ -81,10 +87,28 @@
  * @type number
  * @default 1
  * @desc メニューを閉じた時の継ぎ目を隠すフレーム数です。
+ * 
+ * @param SceneList
+ * @text シーン名のリスト
+ * @type string[]
+ * @default ["Scene_Menu"]
+ * @desc マスク処理を行うシーンの一覧です。
  */
 (function() {
 "use strict";
 
+/**
+ * ●構造体をJSで扱えるように変換
+ */
+function parseStruct1(arg) {
+    var ret = [];
+
+    JSON.parse(arg).forEach(function(str) {
+        ret.push(str);
+    });
+
+    return ret;
+}
 function toNumber(str, def) {
     if (str == undefined || str == "") {
         return def;
@@ -95,6 +119,7 @@ function toNumber(str, def) {
 const PLUGIN_NAME = "NRP_ReturnMenuMask";
 const parameters = PluginManager.parameters(PLUGIN_NAME);
 const pMaskFrame = toNumber(parameters["MaskFrame"], 1);
+const pSceneList = parseStruct1(parameters["SceneList"]);
 
 /**
  * ●シーン開始時
@@ -103,21 +128,26 @@ const _Scene_Map_start = Scene_Map.prototype.start;
 Scene_Map.prototype.start = function() {
     _Scene_Map_start.apply(this, arguments);
 
-    // メニューから戻った際
-    if (SceneManager.isPreviousScene(Scene_Menu)) {
-        this.createReturnMenuMask();
+    // 前画面のクラス名を取得
+    const previousClassName = SceneManager._previousClass.name;
+    for (const sceneName of pSceneList) {
+        // 前画面のクラス名と一致するなら
+        if (previousClassName == sceneName) {
+            this.createReturnMask();
+            return;
+        }
     }
 };
 
 /**
- * 【独自】メニューから戻った際に元画面をキャプチャーした画像でマスク
+ * 【独自】指定シーンから戻った際に元画面をキャプチャーした画像でマスク
  */
-Scene_Map.prototype.createReturnMenuMask = function() {
-    this._returnMenuMaskSprite = new Sprite();
-    this._returnMenuMaskSprite.bitmap = SceneManager.backgroundBitmap();
-    this.addChild(this._returnMenuMaskSprite);
-    this._returnMenuMaskSprite.opacity = 255;
-    this._returnMenuMaskSprite.count = 0;
+Scene_Map.prototype.createReturnMask = function() {
+    this._returnMaskSprite = new Sprite();
+    this._returnMaskSprite.bitmap = SceneManager.backgroundBitmap();
+    this.addChild(this._returnMaskSprite);
+    this._returnMaskSprite.opacity = 255;
+    this._returnMaskSprite.count = 0;
 };
 
 /**
@@ -128,11 +158,11 @@ Scene_Map.prototype.update = function() {
     _Scene_Map_update.apply(this, arguments);
 
     // マスクが存在する場合
-    if (this._returnMenuMaskSprite) {
-        this._returnMenuMaskSprite.count++;
+    if (this._returnMaskSprite) {
+        this._returnMaskSprite.count++;
         // 指定のカウント数が過ぎれば削除
-        if (this._returnMenuMaskSprite.count >= pMaskFrame) {
-            this.removeChild(this._returnMenuMaskSprite);
+        if (this._returnMaskSprite.count >= pMaskFrame) {
+            this.removeChild(this._returnMaskSprite);
         }
     }
 }
