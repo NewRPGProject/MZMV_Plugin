e37b6e7d3df6178317000d8ab86e29043c2596de
Takeshi Sunagawa <74476616+NewRPGProject@users.noreply.github.com>
2023-04-02 16:52:50 +0900

    バージョン表記を調整

--- NRP_AdditionalCCScene.js
+++ NRP_AdditionalCCScene.js
@@ -3,7 +3,7 @@
 //=============================================================================
 /*:
  * @target MZ
- * @plugindesc v1.023 Implemented a class change screen for multiple classes.
+ * @plugindesc v1.03 Implemented a class change screen for multiple classes.
  * @author Takeshi Sunagawa (http://newrpg.seesaa.net/)
  * @base NRP_AdditionalClasses
  * @orderAfter NRP_AdditionalClasses
@@ -417,7 +417,7 @@
 
 /*:ja
  * @target MZ
- * @plugindesc v1.023 多重職業用の転職画面を実装。
+ * @plugindesc v1.03 多重職業用の転職画面を実装。
  * @author 砂川赳（http://newrpg.seesaa.net/）
  * @base NRP_AdditionalClasses
  * @orderAfter NRP_AdditionalClasses
