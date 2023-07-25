5e527047365636b1450362cda73d58d697d86f48
Takeshi Sunagawa <74476616+NewRPGProject@users.noreply.github.com>
2023-07-25 17:31:42 +0900

    ＭＶ対応

--- NRP_MessageWindow.js
+++ NRP_MessageWindow.js
@@ -3,7 +3,7 @@
 //=============================================================================
 /*:
  * @target MV MZ
- * @plugindesc v1.01 Adjust the message window.
+ * @plugindesc v1.011 Adjust the message window.
  * @author Takeshi Sunagawa (http://newrpg.seesaa.net/)
  * @url https://newrpg.seesaa.net/article/492543897.html
  *
@@ -150,7 +150,7 @@
 
 /*:ja
  * @target MV MZ
- * @plugindesc v1.01 メッセージウィンドウを調整する。
+ * @plugindesc v1.011 メッセージウィンドウを調整する。
  * @author 砂川赳（http://newrpg.seesaa.net/）
  * @url https://newrpg.seesaa.net/article/492543897.html
  *
@@ -515,9 +515,11 @@ Window_Message.prototype.updatePlacement = function() {
         // 描画領域を更新
         // これをやらないと文字が表示されなくなる。
         this.createContents();
-        // サイズを文章の表示領域へ反映
-        // これをやらないと改ページ判定がおかしくなる。
-        this.contents.resize(this.width, this.height)
+        if (Utils.RPGMAKER_NAME == "MZ") {
+            // サイズを文章の表示領域へ反映
+            // これをやらないと改ページ判定がおかしくなる。
+            this.contents.resize(this.width, this.height);
+        }
     }
 
     // 元の処理
