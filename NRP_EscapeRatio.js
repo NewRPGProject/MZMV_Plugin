dc6e3c43484da2bfbd71b3d219d771928b9207a2
Takeshi Sunagawa <74476616+NewRPGProject@users.noreply.github.com>
2023-07-31 18:59:19 +0900

    アップロード誤りにつき再更新

--- NRP_EscapeRatio.js
+++ NRP_EscapeRatio.js
@@ -3,55 +3,65 @@
 //=============================================================================
 /*:
  * @target MV MZ
- * @plugindesc v1.00 逃走成功率を変更する。
+ * @plugindesc v1.00 Change the success rate of escape.
  * @author Takeshi Sunagawa (https://newrpg.seesaa.net/)
  * @orderAfter NRP_CountTimeBattle
  * @url https://newrpg.seesaa.net/article/500211133.html
  *
- * @help 逃走成功率の計算式を変更します。
+ * @help Change the formula for calculating the escape success rate.
  * 
- * また、逃走に成功しやすくなる職業やパッシブスキルなども設定できます。
- * 
- * -------------------------------------------------------------------
- * ■既定の計算式
- * -------------------------------------------------------------------
- * 前提として、ツクールＭＺの逃走成功率は以下の通りです。
- * 
- * 逃走成功率 = (0.5 * $gameParty.agility()) / $gameTroop.agility()
- * 
- * 意味は『(0.5 * 味方の敏捷性の平均値) / 敵の敏捷性の平均値』となります。
- * 
- * 例えば、以下のようになります。
- * 
- * ・味方の敏捷性と敵の敏捷性が等しい場合は確率５０％。
- * ・味方の敏捷性が敵の敏捷性のニ倍の場合は確率１００％。
+ * - Calculation can be changed to luck or level calculation
+ * - Minimum escape rate can be set.
+ * - Can be changed to compensate for unsuccessful escape attempts
+ * - Able to set traits that make escape more likely to succeed.
  * 
  * -------------------------------------------------------------------
- * ■計算式の変更
+ * [Default Formula]
  * -------------------------------------------------------------------
- * 上記を参考にプラグインパラメータの『逃走計算式』を変更してください。
+ * The assumption is that
+ * RPG Maker MZ has the following success rates for escapes.
  * 
- * また、当プラグインの機能により、以下のパラメータを計算に使用できます。
+ * Rate = (0.5 * $gameParty.agility()) / $gameTroop.agility()
  * 
- * $gameParty.luck() | $gameTroop.luck()：運の平均値
- * $gameParty.level() | $gameTroop.level()：レベルの平均値
- * $gameParty.maxAgility() | $gameTroop.maxAgility()：敏捷性の最大値
- * $gameParty.maxLuck() | $gameTroop.maxLuck()：運の最大値
- * $gameParty.maxLevel() | $gameTroop.maxLevel()：レベルの最大値
+ * It means
+ * (0.5 * average of actor agility) / average of enemy agility.
  * 
- * 対応しているのは敏捷性と運とレベルだけです。
- * 敵のレベルは標準では存在しないため、外部プラグインで設定してください。
+ * For example
+ * 
+ * - If the actor's agility is equal to the enemy's agility,
+ *   the probability is 50%.
+ * - If the actor's agility is twice the enemy's agility,
+ *   the probability is 100%.
+ * 
+ * -------------------------------------------------------------------
+ * [Change Formula]
+ * -------------------------------------------------------------------
+ * Change the plugin parameter
+ * "EscapeFormula" with reference to the above.
+ * 
+ * The plugin's functionality also allows
+ * the following parameters to be used in the calculation
+ * 
+ * $gameParty.luck() | $gameTroop.luck() : Average of luck
+ * $gameParty.level() | $gameTroop.level() : Average of Level
+ * $gameParty.maxAgility() | $gameTroop.maxAgility() : Max of agility
+ * $gameParty.maxLuck() | $gameTroop.maxLuck() : Max of luck
+ * $gameParty.maxLevel() | $gameTroop.maxLevel() : Max of Level
+ * 
+ * Only agility, luck and level are supported.
+ * Enemy levels do not exist by default
+ * and must be set by an external plugin.
  * 
  * -------------------------------------------------------------------
  * [Note (actor, class, equipment, state, and skill)]
  * -------------------------------------------------------------------
- * 以下を記入すれば、逃走成功率が上昇する特徴を作成できます。
- * スキルについては、習得しているだけで効果が発生します。
+ * You can create a trait that increases your escape success rate
+ * by filling in the following.
+ * For skills, the effect occurs only when they are learned.
  * 
  * <EscapeBonus:0.1>
- * 逃走成功率が10%上昇します。
- * 
- * なお、効果は重複します。
+ * The escape success rate is increased by 10%.
+ * Note that the effect is duplicated.
  * 
  * -------------------------------------------------------------------
  * [Terms]
@@ -67,32 +77,25 @@
  * @-----------------------------------------------------
  * 
  * @param EscapeFormula
- * @text 逃走計算式
  * @type string
  * @default (0.5 * $gameParty.agility()) / $gameTroop.agility()
- * @desc 逃走率を決める計算式です。
- * 既定値：(0.5 * 味方の敏捷性の平均) / 敵の敏捷性の平均
+ * @desc Formula for determining the escape rate. Default: (0.5 * average of actor agility) / average of enemy agility
  * 
  * @param EscapeFailBonus
- * @text 逃走失敗時のボーナス
  * @type number
  * @decimals 2
  * @default 0.1
- * @desc 逃走失敗時、次の成功率が指定数値分だけ上昇します。
- * 初期値は0.1（10%）です。
+ * @desc Upon an failed escape, the following success rates are added. The initial value is 0.1 (10%).
  * 
  * @param EscapeMinRate
- * @text 最低逃走率
  * @type number
  * @decimals 2
- * @desc 最低限保証される逃走率です。
+ * @desc This is the minimum guaranteed escape rate.
  * 
  * @param LimitAlive
- * @text 生存者に限定
  * @type boolean
  * @default false
- * @desc 逃走率の計算を生存者の能力だけに限定します。
- * なお、先制＆奇襲率の計算も生存者が対象になります。
+ * @desc Limit the calculation of the escape rate to the survivor's ability to escape only.
  */
 
 /*:ja
@@ -104,7 +107,10 @@
  *
  * @help 逃走成功率の計算式を変更します。
  * 
- * また、逃走に成功しやすくなる職業やパッシブスキルなども設定できます。
+ * ・運やレベルでの計算に変更可能
+ * ・最低逃走率を設定可能
+ * ・逃走失敗時の補正を変更可能
+ * ・逃走に成功しやすくなる特徴を設定可能
  * 
  * -------------------------------------------------------------------
  * ■既定の計算式
@@ -144,7 +150,6 @@
  * 
  * <EscapeBonus:0.1>
  * 逃走成功率が10%上昇します。
- * 
  * なお、効果は重複します。
  * 
  * -------------------------------------------------------------------
@@ -238,7 +243,6 @@ BattleManager.processEscape = function() {
     // 逃走する度に毎回確率を計算する。
     // ※BattleManager.onEscapeFailureの失敗時補正は消滅する。
     this.makeEscapeRatio();
-alert(this._escapeRatio);
     const ret = _BattleManager_processEscape.apply(this, arguments);
 
     // 逃走失敗回数加算
